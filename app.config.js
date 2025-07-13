0bb4b8a157f2d93a6a73869a41decdff8935330d
B1ackSwan <b1ackswan@Mac.lan>
2025-07-13 18:13:50 -0500

    chore: Bump version to 0.2.10.1 for build regression fix test

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "VerseNotes",
     slug: "VerseNotes",
-    version: "0.2.10",
+    version: "0.2.10.1",
     orientation: "default",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
